c05d9f93a0f4b042ed2d3225a896a4f2aa52a5c6
Jiu <imjiujiujiu@gmail.com>
2025-11-23 15:36:07 +0800

    update navbar styles

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,15 @@
 $(document).ready(function () {
-  //   function setNavbarDisplay() {
-  //     if ($(window).width() < 768) {
-  //       $("nav .navbar").hide();
-  //     } else {
-  //       $("nav .navbar").show();
-  //     }
-  //   }
-
-  //   // 初始執行
-  //   setNavbarDisplay();
-  //   // 當視窗調整大小時也要偵測
-  //   $(window).resize(function () {
-  //     setNavbarDisplay();
-  //   });
+  $(window).on("scroll resize", function () {
+    if ($(window).width() < 768) {
+      if ($(window).scrollTop() > 80) {
+        $("header").addClass("sticky");
+      } else {
+        $("header").removeClass("sticky");
+      }
+    } else {
+      $("header").removeClass("sticky");
+    }
+  });
 
   $("nav button").click(function () {
     $("nav").toggleClass("open");
